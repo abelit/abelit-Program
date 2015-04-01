f0543cf2ef338c5b7179aad056dbb76ebe24523b
abelit <ychenid@live.com>
2015-04-01 20:31:48 +0800

    add python file

---
+++ Python/sendemail_http.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3  
+#coding: utf-8  
+import smtplib  
+from email.mime.text import MIMEText  
+  
+sender = 'ychenid@126.com'  
+receiver = 'chenyingzju@126.com'  
+subject = 'python email test'  
+smtpserver = 'smtp.126.com'  
+username = 'ychenid@126.com'  
+password = 'chen15285649896.'  
+ 
+msg = MIMEText('<html><h1>你好,我是陈英。</h1></html>','html','utf-8')  
+ 
+msg['Subject'] = subject  
+ 
+smtp = smtplib.SMTP()  
+smtp.connect('smtp.126.com')  
+smtp.login(username, password)  
+smtp.sendmail(sender, receiver, msg.as_string())  
+smtp.quit() 
