20cfb80cca5a9daf52e01f4922b83ecb541347f0
abelit <ychenid@live.com>
2015-04-01 17:24:56 +0800

    Update factorial.py
    in python3+ use "print(factorial_of_user_input)"  instead of  "print factorial_of_user_input"  "eval(input("Enter a non-negative integer to take the factorial of:"))" instead of  "input("Enter a non-negative integer to take the factorial of:")"

--- Python/factorial.py
+++ Python/factorial.py
@@ -3,6 +3,6 @@ def factorial(number):
     for i in range(number):
         product=product*(i+1)
     return product
-user_input=input("Enter a non-negative integer to take the factorial of:")
+user_input=eval(input("Enter a non-negative integer to take the factorial of:"))
 factorial_of_user_input=factorial(user_input)
-print factorial_of_user_input
+print(factorial_of_user_input)
