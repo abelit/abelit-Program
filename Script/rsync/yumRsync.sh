8e827bdb36193e90bd227db9e7e39360ef5a5ffc
abelit <ychenid@live.com>
2015-09-01 08:55:11 +0800

    modify yumRsync.sh

--- Script/rsync/yumRsync.sh
+++ Script/rsync/yumRsync.sh
@@ -27,8 +27,10 @@ sourceList="centos_7 epel epel_testing openstack_juno openstack_kilo"
 function check() {
 	if [ $? -eq 0 ];then
 		echo -e "\033[1;32mRsync is success!\033[0m" >>$LogFile/$Date.log
+		echo -e "\033[1;32mRsync is success!\033[0m"
 	else
 		echo -e "\033[1;31mRsync is fail!\033[0m" >>$LogFile/$Date.log
+		echo -e "\033[1;31mRsync is fail!\033[0m"
 	fi
 }
 
@@ -51,19 +53,27 @@ function rsyncStart() {
 			if [ $slist = "centos_7" ];then
 				echo "Now start to rsync $slist" >>$LogFile/$Date.log
 				$RsyncBin $RsyncPerm  --exclude isos $scentos_7 $centos_7 >>$LogFile/$Date.log
-				check
+				centoscheck="`check`"
 			elif [ $slist = "epel" ];then
 				echo "Now start to rsync $slist" >>$LogFile/$Date.log
 				$RsyncBin $RsyncPerm --exclude ppc64  $sepel $epel >>$LogFile/$Date.log
-				check
+				epelcheck="`check`"
 			elif [ $slist = "epel_testing" ];then
 				echo "Now start to rsync $slist" >>$LogFile/$Date.log
 				$RsyncBin $RsyncPerm  --exclude ppc64 $sepel_testing $epel_testing >>$LogFile/$Date.log
-				check
+				epel_testingcheck="`check`"
 			fi
 		done
+	echo ""
+	echo "----------------------------------------------------------------------------------------"
+	echo "yum-packages rsync hava been finished!" &&  echo "yum-packages rsync hava been finished!" >>$LogFile/$Date.log
+	echo "The detail of all yum-packages:" &&   echo "The detail of all yum-packages:" >>$LogFile/$Date.log
+	echo "The centos rsync status:$centoscheck" && echo "The centos rsync status:$centoscheck" >>$LogFile/$Date.log
+	echo "The epel rsync status:$epelcheck" && echo "The epel rsync status:$epelcheck" >>$LogFile/$Date.log
+	echo "The epel_testing rsync status:$epel_testingcheck" &&  echo "The epel_testing rsync status:$epel_testingcheck" >>$LogFile/$Date.log
+	echo "----------------------------------------------------------------------------------------"
+	
 }
 
 #call rsyncStart
 rsyncStart
-
